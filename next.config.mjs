123c743ca58d9e11e82861e867e7a0498e2faca4
rishi1123d <rishi.kanaparti@gmail.com>
2025-05-19 02:32:36 -0400

    c

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-    disableStaticImages: true,
     remotePatterns: [],
-    formats: ['image/jpeg', 'image/png', 'image/webp', 'image/svg+xml'],
-    minimumCacheTTL: 60,
   },
   output: 'export',
   // Set trailing slash to true for better compatibility with static hosting
